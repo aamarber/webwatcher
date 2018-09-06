b7deeb89fc0223be715f27571fe2817e6f7e8208
AzureAD\AarónMartin <amartin@customerville.com>
2018-09-06 10:32:05 +0200

    Using html agility pack to select a node by css and a diff engine to show the differences.

--- WebWatcher/Program.cs
+++ WebWatcher/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Net;
 using System.Threading;
+using DiffPlex;
+using DiffPlex.DiffBuilder;
+using DiffPlex.DiffBuilder.Model;
+using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
 
 namespace WebWatcher
 {
@@ -10,9 +15,31 @@ namespace WebWatcher
         {
             string previousWebResult = string.Empty;
 
-            var url = "http://www.ceice.gva.es/auto/Actas/126_AUDICIO%20I%20LLENG/";
+            string url = null,
+                cssSelector = null;
 
-            Console.WriteLine("Started checking the website");
+            if (args.Length > 0)
+            {
+                url = args[0];
+            }
+            if (args.Length > 1)
+            {
+                cssSelector = args[1];
+            }
+
+            if (string.IsNullOrEmpty(url))
+            {
+                Console.WriteLine("Type the url you want to check: ");
+                url = Console.ReadLine();
+            }
+
+            if (string.IsNullOrEmpty(cssSelector))
+            {
+                Console.WriteLine("Type the css selector (empty if none): ");
+                cssSelector = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Started checking {url}");
 
             while (true)
             {
@@ -25,23 +52,77 @@ namespace WebWatcher
                 if (string.IsNullOrEmpty(previousWebResult))
                 {
                     previousWebResult = nextWebResult;
+                    Wait();
+                    continue;
                 }
+
+                var previousDoc = new HtmlDocument();
+                previousDoc.LoadHtml(previousWebResult);
+
+                var nextDoc = new HtmlDocument();
+                nextDoc.LoadHtml(nextWebResult);
+
+                var previousContent = previousDoc.DocumentNode.QuerySelector(cssSelector);
+
+                var nextContent = nextDoc.DocumentNode.QuerySelector(cssSelector);
 
                 if (nextWebResult != previousWebResult)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
 
-                    Console.WriteLine("WEB WASN'T THE SAME " + DateTime.Now);
+                    if (ShowDifferences(previousContent, nextContent))
+                    {
+                        Console.Beep(800, 5000);
 
-                    Console.Beep(800, 5000);
+                        previousWebResult = nextWebResult;
+                    }
 
                     Console.ForegroundColor = ConsoleColor.White;
-
-                    previousWebResult = nextWebResult;
                 }
 
-                Thread.Sleep(1000 * 30);
+                Wait();
             }
+        }
+
+        private static bool ShowDifferences(HtmlNode previousContent, HtmlNode nextContent)
+        {
+            var diffBuilder = new InlineDiffBuilder(new Differ());
+            var diff = diffBuilder.BuildDiffModel(previousContent.InnerHtml, nextContent.InnerHtml);
+
+            if(diff.Lines.TrueForAll(x => x.Type == ChangeType.Unchanged))
+            {
+                return false;
+            }
+
+            Console.WriteLine($"WEB WASN'T THE SAME {DateTime.Now}");
+
+            foreach (var line in diff.Lines)
+            {
+                switch (line.Type)
+                {
+                    case ChangeType.Inserted:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("+ ");
+                        break;
+                    case ChangeType.Deleted:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("- ");
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("  ");
+                        break;
+                }
+
+                Console.WriteLine(line.Text);
+            }
+
+            return true;
+        }
+
+        private static void Wait()
+        {
+            Thread.Sleep(1000 * 30);
         }
     }
 }
